c4321b8ca2e4a2d97c4787e40403d11573e3da74
Iggy <igoririanto@rocketmail.com>
2017-12-07 11:28:56 -0800

    remove axios

--- src/Modal.js
+++ src/Modal.js
@@ -2,25 +2,12 @@ import './modal.scss';
 import JSXComponent from 'metal-jsx';
 import axios from 'axios';
 
-var data = {rss_url: 'https://medium.com/feed/@stupendous_igg'}
+
 
 class Modal extends JSXComponent {
 
-	attached(){
-		var that = this;
-		axios.get('https://api.rss2json.com/v1/api.json', {
-				params: data
-			})
-			.then(function (response) {
-				console.log(response.data.items);
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-	}
-
 	render() {
-		console.log(this.state.axiosResponse)
+
 		return (
 			<div class="my-modal">
 				<h2>Hello World!</h2>
@@ -30,12 +17,4 @@ class Modal extends JSXComponent {
 	}
 }
 
-Modal.STATE = {
-	isLoading: {
-		value: true
-	},
-	axiosResponse: {
-		value: {}
-	}
-}
 export default Modal;
